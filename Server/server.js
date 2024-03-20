700cb04a8ccff7448eaf1fef218f5938c3f4f246
shahina42 <141030593+shahina42@users.noreply.github.com>
2024-03-20 17:25:47 +0530

    Server setup Done

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.get('/ping',(req,res)=>{
+    res.json({
+        message:"pong"
+    })
+})
+
+if(require.main === module){
+    app.listen(port,()=>{
+        console.log(`server running on PORT: ${port}`)
+    })
+}
+module.exports=app;
